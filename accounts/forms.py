6a3deb73f9252cb2218f71f59eb57138b5830a41
Enrique Corpus <enriquecorpus@skydev.solutions>
2022-09-23 09:54:33 +0800

    remove help text on register page

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,6 +10,12 @@ class MyUserCreationForm(UserCreationForm):
         model = accounts.models.MyUser
         fields = ('username', 'email', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super(MyUserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     def save(self, commit=True):
         user = super(MyUserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
--- accounts/views.py
+++ accounts/views.py
@@ -3,15 +3,20 @@ from .forms import MyUserCreationForm
 from django.contrib.auth import login
 from django.contrib import messages
 
+_POST = "POST"
+_GET = "GET"
+
 
 def register(request):
-    if request.method == "POST":
+    if request.method == _POST:
         form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful.")
             return redirect("main:homepage")
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+        for error in form.errors:
+            for err in form.errors[error]:
+                messages.error(request, err)
     form = MyUserCreationForm()
     return render(request=request, template_name="auth/register.html", context={"form": form})
